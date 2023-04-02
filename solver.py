98188e5acd96ae0e1c987975bd952a3b3df34e64
cheesy94 <97102775+cheesy94@users.noreply.github.com>
2023-04-02 19:46:48 +0200

    Unique position in row and col added

--- solver.py
+++ solver.py
@@ -1,14 +1,28 @@
 import numpy as np
 
 
-## Get small grid to which number i,j belongs
+## Get small grid index to which cell i,j belongs
+def get_subgrid_index(i,j,size):
+    
+    subgrid_i = int(i/size)*size
+    subgrid_j = int(j/size)*size
+    
+    return subgrid_i,subgrid_j
+
+## Get indexes of complementary small grids
+def get_complementary_subgrid_index(i,j,size):
+    
+    si,sj = get_subgrid_index(i,j,size)
+    
+    return [(i*size,j*size) for i in range(size) for j in range(size) if bool(i==si) ^ bool(j==sj)]
+
+## Get small grid i,j
 def get_subgrid_matrix(matrix,i,j):
     subgrid_size = int(np.sqrt(np.sqrt(matrix.size)))
     
-    subgrid_i = int(i/subgrid_size)*subgrid_size
-    subgrid_j = int(j/subgrid_size)*subgrid_size
+    si,sj = get_subgrid_index(i,j,subgrid_size)
     
-    return matrix[subgrid_i:subgrid_i+subgrid_size,subgrid_j:subgrid_j+subgrid_size]
+    return matrix[si:si+subgrid_size,sj:sj+subgrid_size]
 
 
 ## Get possible numbers for every cell
@@ -26,12 +40,12 @@ def get_possible_numbers(matrix):
                     continue
                 
                 for i in range(rowcol_size):
-                    # Check row
-                    if n in matrix[i,:]:
-                        continue
-                    
                     # Already solved
                     if matrix[i,j] != 0:
+                        continue
+                    
+                    # Check row
+                    if n in matrix[i,:]:
                         continue
                     
                     # Check subgrid
@@ -42,6 +56,8 @@ def get_possible_numbers(matrix):
     
     return possible
 
+
+## Check if possible number prevents other subgrid from
 
 ## Solver
 def solve(initial):
@@ -67,31 +83,44 @@ def solve(initial):
         # Get possible numbers for every cell
         possible = get_possible_numbers(current)
         
+        # Filter possible numbers
+        
         # Solve sure numbers
         for j in range(rowcol_size):
             for i in range(rowcol_size):
                 p = possible[i,j]
                 lenp = len(p)
                 
-                if lenp==0:
+                # Already solved
+                if current[i,j] != 0: #lenp==0:
                     continue
                 
+                # Unique number in cell
                 if lenp==1:
                     current[i,j] = p[0]
                     #print(i,j,current[i,j])
                     continue
                 
+                # Neighbours
                 possible_neighbours = possible.copy()
                 possible_neighbours[i,j] = []
                 
-                subgrid_possible = get_subgrid_matrix(possible_neighbours,i,j)
-                subgrid_possible = np.unique(np.concatenate(subgrid_possible.flatten()))
-                #subgrid_possible_numbers,subgrid_possible_counts = np.unique(np.concatenate(subgrid_possible.flatten()),return_counts=True)
-                #subgrid_secure_numbers = subgrid_possible_numbers[subgrid_possible_counts==1]
-                
-                for pn in p:
-                    if not pn in subgrid_possible:
+                for ipn,pn in enumerate(p):
+                    
+                    # Unique position in row, col, subgrid
+                    possible_row = np.unique(np.concatenate(possible_neighbours[i,:].flatten()))
+                    possible_col = np.unique(np.concatenate(possible_neighbours[:,j].flatten()))
+                    possible_subgrid = np.unique(np.concatenate(get_subgrid_matrix(possible_neighbours,i,j).flatten()))
+                    if (not pn in possible_row) or (not pn in possible_col) or (not pn in possible_subgrid):
                         current[i,j] = pn
-                        #print(i,j,current[i,j])
+                        #p.pop(ipn)
                         
+                    # Blocks number in other submatrix
+                    #current.copy()
+                    #possible_
+                    #for sij in get_complementary_subgrid_index(i,j,int(np.sqrt(rowcol_size))):
+                    #    current_subgrid = get_subgrid_matrix(possible,*sij)
+                        #if True:
+                        #    p.pop(ipn)
+                
     return current,loops,possible
